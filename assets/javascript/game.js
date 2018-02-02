9b103153390a4757f9697a507093dac268b4862c
Tyler Schmauch <tschmauch@hotmail.com>
2018-02-01 21:32:29 -0700

    little jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// Wait for the page to load
+$(document).ready(function () {
+
+// Detect key presses
+$(document).on('keyup', function(event) {
+	event.key
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
