f54e9ab1713d5bbd5060dc1e438fe58062cc8890
Tyler Schmauch <tylers@kornerstoneadmin.com>
2018-02-02 17:11:54 -0700

    movie array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,47 @@
+var movieArray = ['Frankenstein',
+'King Kong',
+'Snow White and the Seven Dwarfs',
+'The Wizard of Oz',
+'Pinocchio',
+'Citizen Kane',
+'Casablanca',
+'Psycho',
+'Lawrence of Arabia',
+'The Godfather',
+'Taxi Driver',
+'Alien',
+'E.T. The Extra-Terrestrial',
+'L.A. Confidential',
+'Toy Story 2',
+'Finding Nemo',
+'The Dark Knight',
+'Up',
+'Toy Story 3',
+'Mad Max: Fury Road',
+'Inside Out',
+'Spotlight',
+'Star Wars: Episode VII - The Force Awakens',
+'Zootopia',
+'Arrival',
+'Hell or High Water',
+'Manchester by the Sea',
+'The Jungle Book',
+'The Big Sick',
+'Dunkirk',
+'Lady Bird',
+'Wonder Woman',
+'Logan',
+'Baby Driver',
+'War for the Planet of the Apes',
+'The Post',
+'Interstellar'
+]
+
+
+
+
+
+
 // Wait for the page to load
 $(document).ready(function () {
 
